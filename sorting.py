5df08d53dfe94145933c937201eef4764f3a5cb3
Aarav <aaravsubberwal8@gmail.com>
2024-11-27 14:42:48 +0530

    THE QUOTE

--- sorting.py
+++ sorting.py
@@ -6,6 +6,8 @@ A visual sorter for all the sorts in python.
 
 I've only done this in vscode terminal and I've set up the required workspace settings already in settings.json. Can't gaurantee if it'll work on other  
 IDE's. Run the file in dedicated terminal and maximize it's size.
+
+"I can't sort my life but I can sort some DATA and show it off"
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import random
 import time
